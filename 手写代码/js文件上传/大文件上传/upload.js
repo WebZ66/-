3e35562036ac012036ca30b528d2948f4ca08ac1
zhengdashun <13255183458zds@FT>
2023-11-27 17:11:52 +0800

    大文件上传，断点续传

---
+++ 手写代码/js文件上传/大文件上传/upload.js
@@ -0,0 +1,1 @@
+import sparkMd5 from '大文件上传/node_modules/spark-md5/spark-md5'
---
+++ 手写代码/js文件上传/大文件上传/web-worker/worker.js
@@ -0,0 +1,28 @@
+self.importScripts('../spark-md5.js')
+
+self.onmessage = function (event) {
+    hash(event.data).then((v) => {
+        self.postMessage(v)
+    })
+    function hash(chunks) {
+        return new Promise((resolve) => {
+            const spark = new SparkMD5()
+            function _read(i) {
+                if (i >= chunks.length) {
+                    //读取了所有的块 需要调用spark.end(),返回的hash值
+                    resolve(spark.end())
+                    return
+                }
+                const reader = new FileReader()
+                reader.readAsArrayBuffer(chunks[i])
+                reader.onload = function (e) {
+                    //读取到了对应块的字节
+                    let bytes = e.target.result
+                    spark.append(bytes)
+                    _read(i + 1)
+                }
+            }
+            _read(0)
+        })
+    }
+}
